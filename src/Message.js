9226311b67927b6ba2f2461bc43af5f2708ec3be
Stanley Lee <45775140+96StanleyLee@users.noreply.github.com>
2020-12-13 23:16:01 -0500

    added image copy pasting url!

--- src/Message.js
+++ src/Message.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Message.css";
 import { Avatar } from "@material-ui/core";
 import moment from "moment-timezone";
+import { useValidateImageURL } from "use-validate-image-url";
 
 function Message({ message }) {
+  let [status, setStatus] = useState("");
+
+  let imageStatus = useValidateImageURL(message.messageData.message);
+
+  useEffect(() => {
+    if (imageStatus === "valid") {
+      setStatus("valid");
+    }
+  }, [imageStatus]);
+
   let timeStamp = "";
   if (message.messageData.timestamp !== null) {
     timeStamp = moment(new Date(message.messageData.timestamp.toDate()));
     timeStamp = timeStamp.tz("America/New_York").format("MM/DD/YYYY h:mm A");
   }
+
   return (
     <div className="message">
       <Avatar src={message.messageData.user.photo} />
@@ -17,7 +29,14 @@ function Message({ message }) {
           {message.messageData.user.displayName}
           <span className="message__timestamp">{timeStamp}</span>
         </h4>
-        <p> {message.messageData.message} </p>
+        {status === "valid" ? (
+          <img
+            style={{ maxWidth: "500px", maxHeight: "500px" }}
+            src={message.messageData.message}
+          />
+        ) : (
+          <p> {message.messageData.message} </p>
+        )}
       </div>
     </div>
   );
